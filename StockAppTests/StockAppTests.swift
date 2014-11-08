6bc69b3ae42aea786e4fc3a8f000d33e1f50d32e
Andrew Hansen <hansenandrewg@gmail.com>
2014-11-08 11:26:37 -0500

    Initial commit

---
+++ StockAppTests/StockAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  StockAppTests.swift
+//  StockAppTests
+//
+//  Created by Andrew Hansen on 11/8/14.
+//  Copyright (c) 2014 Andrew Hansen. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class StockAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
